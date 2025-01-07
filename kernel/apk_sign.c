3eeb9496faafcded3c96c63e8360ba4392c66a03
RoHaN <82774471+reaPeR1010@users.noreply.github.com>
2025-01-07 19:19:53 +0530

    Update apk_sign.c

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -318,6 +318,5 @@ bool ksu_is_manager_apk(char *path)
 {
 	return (check_v2_signature(path, 0x363, "4359c171f32543394cbc23ef908c4bb94cad7c8087002ba164c8230948c21549") // dummy.keystore
 	|| check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH)  // ksu official
-/*	|| 
-	);
+	|| check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")); // 5ec1cff
 }
